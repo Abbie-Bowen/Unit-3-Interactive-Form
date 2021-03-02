ad0f97ea7da1cce736e32d266604e9fea4b17748
abmarie <77949813+abmarie@users.noreply.github.com>
2021-03-02 08:06:36 -0800

    still working on validating registration

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -7,33 +7,36 @@ const otherJobRoleInput = document.querySelector('#other-job-role');
 const shirtDesignInput = document.querySelector('#design');
 const shirtColorInput =document.querySelector('#shirt-colors');
 const registerForActivities = document.querySelector('#activities');
+const allActivities = document.querySelectorAll('#activities input');
 const paymentMethodInput = document.querySelector('#payment');
 const expirationMonthInput = document.querySelector('#exp-month');
 const expirationYearInput = document.querySelector('#exp-year');
 const creditCardNumberInput = document.querySelector('#cc-num');
 const zipCodeInput = document.querySelector('#zip');
 const cvvInput = document.querySelector('#cvv')
+const formElement = document.querySelector("form");
 
+// BASIC INFO SECTION
 //default focus on the name field
 userNameInput.focus();
 //name valdiation: name field cannot be blank, empty, or contain numbers
 function isValidName (userName) {
-  return /^[a-zA-Z\s]+$/.test(userName);
+  return /^[a-zA-Z][a-zA-Z\s]*$/.test(userName);
 }
-userNameInput.addEventListener("input", createListener(isValidName));
+//real time error messaging
+userNameInput.addEventListener("keyup", createListener(isValidName));
 
 //email validation: a few characters followed by @ followed by a few more characters and a . and more characters
 function isValidEmail(userEmail) {
   return /[^@]+@[^@.]+\.[a-z]+$/i.test(userEmail);
 }
-userEmailInput.addEventListener("input", createListener(isValidEmail));
+//real time error messaging
+userEmailInput.addEventListener("keyup", createListener(isValidEmail));
 
-// //Job Role section
-//   hide the "text field" with the id of "other-job-role" //hide by default
+// JOB ROLE SECTION
+//if other job role is selected, text box appears for user
 otherJobRoleInput.style.display = 'none';
 
-//   job role <select> element listen for user changes
-//   if change, display/hide the "text field" based on selection in drop down menu
 jobRoleInput.addEventListener('change', e => {
   if (e.target.value === 'other') {
     otherJobRoleInput.style.display = 'inherit';
@@ -41,10 +44,9 @@ jobRoleInput.addEventListener('change', e => {
     otherJobRoleInput.style.display = 'none';
   }
 });
-// //no validation requirements
 
-// //TShirt Info section
-// disable "color" <select> element
+// TSHIRT INFO SECTION
+// default disable "color" <select> element
 shirtColorInput.style.display = 'none';
 // Design selection listens for changes and filters color choices based on user selection
 shirtDesignInput.addEventListener('change', e =>{
@@ -55,11 +57,8 @@ shirtDesignInput.addEventListener('change', e =>{
 
 function toggleColors (colorTheme) {
   const allColors = document.querySelectorAll('#color option[data-theme]');
-// QUESTION: How can I pull the data-theme name and run the following using a variable?
-// I was able to find CSS.escape() so I could connect colorTheme into the variable I'm using
-// to select the color options:
-// const colorChoices = document.querySelectorAll('#color option[data-theme="CSS.escape(colorTheme)"]');
-// but this doesn't work in all browsers. Is there a better & more concise way?
+/*QUESTION: How can I pull the data-theme name and run the following using a variable?
+Template literals don't seem to work in querySelectors. Is there a better & more concise way?*/
   const jsPuns = document.querySelectorAll('#color option[data-theme="js puns"]');
   const heartJs = document.querySelectorAll('#color option[data-theme="heart js"]');
   for (i=0; i<allColors.length; i++) {
@@ -78,60 +77,42 @@ function toggleColors (colorTheme) {
   }
 }
 
-// //Register for Activities section
-//   register for activities fieldset listen for changes
-//     when a change=>
-//       if an activity is checked, total cost should increase by the value
-//       in data-cost attribute of the activity's <input type="checkbox"> element
-//
-//       if an activty is unchecked, total cost should decrease by that amount
-//
-//       <p> with id of "activity-cost" updates to reflect new total cost
+/* Register for Activities section: when an activity is selected, the total cost increases by the
+cost of that activity. When an activity is unselected, the total cost decreases by the cost of that
+activity. If an activity is selected that occurs at a conflicting time at another activity, the
+conflicting activity is disabled.*/
 let totalCost = 0;
 registerForActivities.addEventListener('change', e => {
   const activity=e.target;
   const costTotalHTML = document.querySelector('.activities-cost');
-  if (activity.checked) {
-    const activityCost = activity.dataset.cost;
-    totalCost += +activityCost;
-    removeConflictingActivities(activity);
-} if (!activity.checked) {
-    const activityCost = activity.dataset.cost;
-    totalCost -= +activityCost;
-  }
-costTotalHTML.innerHTML=(`Total: $${totalCost}`);
-});
-
-// //form validation
-//   register for activities must have at least one activity selected
-registerForActivities.addEventListener("onfocus", e => {
-  const checkedActivities = document.querySelectorAll('#activities [type="checkbox"]:checked');
-  if (checkedActivities.length=0) {
-    let hint = registerForActivities.lastElementChild;
-    hint.style.display = "inherit";
-  } else {
-    let hint = registerForActivities.lastElementChild;
-    hint.style.display = "none";
-  }
-});
-
-//   //extra Credit--no registering for conflicting Activities
-//   when a user selects an activity, loop over all activities and check if any
-//   have the same day & time as the activity that was just checked/unchecked
-//     as long as the activity was not just checked/unchecked, disable/enable
-//     the conflicting activity's checkbox input and add/remove the 'disabled' className
-//     to the activity's parent label element
-function removeConflictingActivities(activity) {
-  const schedule = activity.dataset.dayAndTime;
-  const allActivities = document.querySelectorAll('#activities input');
-  if (schedule === undefined) {
+  const activityCost = activity.dataset.cost;
+  if (activity.name === "all" && activity.checked) {
     for (i=1; i<allActivities.length; i++) {
       allActivities[i].disabled = true;
       let parentLabel = allActivities[i].parentElement;
       parentLabel.className = 'disabled';
+      allActivities[i].checked = false;
+      totalCost = activityCost;
       }
-    return;
+  } if (activity.name === "all" && !activity.checked) {
+      for (i=1; i<allActivities.length; i++) {
+        allActivities[i].disabled = false;
+        let parentLabel = allActivities[i].parentElement;
+        parentLabel.className = '';
+        totalCost = 0;
+        }
+  } if (activity.name !== "all" && activity.checked) {
+        totalCost += +activityCost;
+        removeConflictingActivities(activity);
+  } if (activity.name !== "all" && !activity.checked) {
+        totalCost -= +activityCost;
+        undoConflicts(activity);
   }
+costTotalHTML.innerHTML=(`Total: $${totalCost}`);
+});
+
+function removeConflictingActivities(activity) {
+  const schedule = activity.dataset.dayAndTime;
   for (i=1; i<allActivities.length; i++) {
     if (allActivities[i].dataset.dayAndTime === schedule && allActivities[i] !== activity) {
       allActivities[i].disabled = true;
@@ -140,6 +121,36 @@ function removeConflictingActivities(activity) {
     }
   }
 }
+
+function undoConflicts(activity) {
+  const schedule = activity.dataset.dayAndTime;
+  const allActivities = document.querySelectorAll('#activities input');
+  for (i=1; i<allActivities.length; i++) {
+    if (allActivities[i].dataset.dayAndTime === schedule && allActivities[i] !== activity) {
+      allActivities[i].disabled = false;
+      let parentLabel = allActivities[i].parentElement;
+      parentLabel.className = '';
+    }
+  }
+}
+
+// //form validation
+//   register for activities must have at least one activity selected
+
+//   const checkedActivities = document.querySelectorAll('#activities [type="checkbox"]:checked');
+//   let hint = document.querySelector('.activities-hint-hint');
+//   let hintBorder = document.querySelector('.activities-box error-border');
+//   if (checkedActivities.length === 0) {
+//     hint.style.display = "inherit";
+//     // hintBorder.style.display = ".not-valid";
+//   } else {
+//     let hint = registerForActivities.lastElementChild;
+//     hint.style.display = "none";
+//     // hintBorder.style.display = "none";
+//   }
+// });
+
+
 // //Payment selection
 // I'm going to pay with <select> element to listen to changes
 //   when a change => hide all payment sections except the selected one
@@ -158,15 +169,7 @@ function removeConflictingActivities(activity) {
 //     check credit card validation, if cc is selected if false return
 //   preventDefault if one of more of the required fields is invalid
 //
-function createListener(validator) {
-  return e => {
-    const text = e.target.value;
-    const valid = validator(text);
-    const showHint = text !== "" && !valid;
-    const element = e.target;
-    showOrHideHint(showHint, element);
-  };
-}
+
 
 //   //accessibility
 //   //focus states
@@ -176,21 +179,31 @@ function createListener(validator) {
 //     if blur=> remove the focus ClassName from the label element that possessess it
 //     (target element with className of .focus)
 
-// obvious error messaging
-function showOrHideHint (show, element) {
-  if (show) {
-    showHint(element);
-  } else {
-    hideHint(element);
-  }
+function createListener(validator) {
+  return e => {
+    const text = e.target.value;
+    const valid = validator(text);
+    const show = !valid;
+    const element = e.target;
+    if (show) {
+      showHint(element);
+    } if (!show) {
+      hideHint(element);
+    }
+  };
 }
+
 // helper function to  update styles when errors are detected
-function showHint (element) {
+function showHint(element) {
   let parentElement = element.parentNode;
-  parentElement.className = '';
-  parentElement.className = "not-valid";
-  let hint = parentElement.lastElementChild;
-  hint.style.display = "inherit";
+  if (parentElement.parentNode.className === ".basic-info") {
+    parentElement.className = '';
+    parentElement.className = "not-valid";
+    let hint = parentElement.lastElementChild;
+    hint.style.display = "inherit";
+  } if (parentElement.parentNode.className === ".activities")  {
+
+  } if (parentElement.parentNode.className === ".num-box")
 }
 // helper function to update styles when errors are resolved
 function hideHint(element) {
